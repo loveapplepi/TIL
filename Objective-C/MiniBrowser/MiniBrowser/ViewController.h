a19945852a950dbeedb85015cc6a629526fe167b
loveapplepi <loveapplepi@naver.com>
2019-05-14 15:49:38 +0900

    update mini browser

--- Objective-C/MiniBrowser/MiniBrowser/ViewController.h
+++ Objective-C/MiniBrowser/MiniBrowser/ViewController.h
@@ -8,11 +8,18 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<UITextFieldDelegate, UIWebViewDelegate>
+
 @property (strong, nonatomic) IBOutlet UISegmentedControl *bookmarkSegmentedControl;
 @property (strong, nonatomic) IBOutlet UIWebView *mainWebView;
 @property (strong, nonatomic) IBOutlet UITextField *textField;
+@property (strong, nonatomic) IBOutlet UIActivityIndicatorView *activityIndicatorView;
 
+- (IBAction)bookmarkAction:(id)sender;
+- (IBAction)backAction:(id)sender;
+- (IBAction)forwardAction:(id)sender;
+- (IBAction)stopAction:(id)sender;
+- (IBAction)refreshAction:(id)sender;
 
 
 @end
--- Objective-C/MiniBrowser/MiniBrowser/ViewController.m
+++ Objective-C/MiniBrowser/MiniBrowser/ViewController.m
@@ -13,11 +13,57 @@
 @end
 
 @implementation ViewController
-
+@synthesize mainWebView, textField, bookmarkSegmentedControl, activityIndicatorView;
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    NSString *urlString = @"https://www.facebook.com";
+    [mainWebView loadRequest:[NSURLRequest requestWithURL: [NSURL URLWithString: urlString]]];
+    textField.text = urlString;
+    textField.delegate = self;
+    mainWebView.delegate = self;
+}
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    NSString *urlString = textField.text;
+    if (![urlString hasPrefix:@"https://"]) {
+        urlString = [[NSString alloc]initWithFormat:@"https://%@",urlString];
+    }
+    [mainWebView loadRequest:[NSURLRequest requestWithURL: [NSURL URLWithString: urlString]]];
+    [textField resignFirstResponder];
+    return YES;
 }
 
+
+- (IBAction)bookmarkAction:(id)sender {
+    NSString *bookmarkURL = [bookmarkSegmentedControl titleForSegmentAtIndex: bookmarkSegmentedControl.selectedSegmentIndex];
+    
+    NSString *urlString = [[NSString alloc]initWithFormat:@"https://www.%@.com", bookmarkURL];
+    [mainWebView loadRequest:[NSURLRequest requestWithURL: [NSURL URLWithString: urlString]]];
+    textField.text = urlString;
+}
+
+- (IBAction)backAction:(id)sender {
+    [mainWebView goBack];
+}
+
+- (IBAction)forwardAction:(id)sender {
+    [mainWebView goForward];
+}
+
+- (IBAction)stopAction:(id)sender {
+    [mainWebView stopLoading];
+}
+
+- (IBAction)refreshAction:(id)sender {
+    [mainWebView reload];
+}
+
+- (void)webViewDidStartLoad:(UIWebView *)webView {
+    [activityIndicatorView startAnimating];
+}
+
+- (void)webViewDidFinishLoad:(UIWebView *)webView {
+    [activityIndicatorView stopAnimating];
+}
 
 @end
