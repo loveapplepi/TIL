8659c4b7b13ffbd17c3f796c3438c05df1b90c92
loveapplepi <loveapplepi@naver.com>
2019-05-14 10:44:38 +0900

    make object-c

---
+++ Objective-C/StartObject-C/StartObject-C/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  StartObject-C
+//
+//  Created by soohyeon on 14/05/2019.
+//  Copyright © 2019 soohyeon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
