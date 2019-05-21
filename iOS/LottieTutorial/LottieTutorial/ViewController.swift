093bd64e0bfe69c414dd90e1bd2f3fd4becce4eb
loveapplepi <loveapplepi@naver.com>
2019-05-22 08:34:57 +0900

    create LottieTutorial

---
+++ iOS/LottieTutorial/LottieTutorial/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  LottieTutorial
+//
+//  Created by loveapplepi on 21/05/2019.
+//  Copyright © 2019 loveapplepi. All rights reserved.
+//
+
+import UIKit
+import Lottie
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+   
+    }
+
+
+}
+
