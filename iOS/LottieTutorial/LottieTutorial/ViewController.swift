d89c49792e500ca18b7029b2205a30aec8153cbf
loveapplepi <loveapplepi@naver.com>
2019-05-22 11:33:20 +0900

    add lottie animation

--- iOS/LottieTutorial/LottieTutorial/ViewController.swift
+++ iOS/LottieTutorial/LottieTutorial/ViewController.swift
@@ -15,6 +15,16 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        let animationView = AnimationView(name: "data")
+        animationView.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height)
+        view.center = self.view.center
+        view.contentMode = .scaleToFill
+        
+        view.addSubview(animationView)
+        animationView.play()
+        
+        animationView.loopMode = .loop // Sets the loop behavior for `play` calls. Defaults to `playOnce`
+        animationView.animationSpeed = 0.5 //Sets the speed of the animation playback. Defaults to 1
    
     }
 
---
+++ iOS/LottieTutorial/README.md
@@ -0,0 +1,15 @@
+## Lottie Animation
+
+<br/>
+
+[After Effects 애니메이션을 JSON 데이터 형식으로 추출](https://airbnb.io/lottie/#/after-effects)해서 애니메이션을 만들수 있다.
+
+
+
+
+
+
+
+### 참고링크
+
+[aribnb_lottie](https://airbnb.io/lottie/#/README)
