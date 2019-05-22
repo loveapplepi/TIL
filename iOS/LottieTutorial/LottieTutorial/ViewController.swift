933e2228468e71d4d3572a95a3a0133ca3530a92
loveapplepi <loveapplepi@naver.com>
2019-05-22 13:13:40 +0900

    add view animation

--- iOS/LottieTutorial/LottieTutorial/ViewController.swift
+++ iOS/LottieTutorial/LottieTutorial/ViewController.swift
@@ -23,8 +23,21 @@ class ViewController: UIViewController {
         view.addSubview(animationView)
         animationView.play()
         
-        animationView.loopMode = .loop // Sets the loop behavior for `play` calls. Defaults to `playOnce`
+        //animationView.loopMode = .loop // Sets the loop behavior for `play` calls. Defaults to `playOnce`
         animationView.animationSpeed = 0.5 //Sets the speed of the animation playback. Defaults to 1
+        
+        let timing = UICubicTimingParameters(animationCurve: .easeInOut)
+        let animator = UIViewPropertyAnimator(duration: 2.0, timingParameters: timing)
+        
+        animator.addAnimations {
+            animationView.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
+        }
+        
+        animator.addCompletion {_ in
+            animationView.transform = CGAffineTransform(scaleX: 1.0, y: 1.0)
+        }
+        
+        animator.startAnimation(afterDelay: 0.3)
    
     }
 
