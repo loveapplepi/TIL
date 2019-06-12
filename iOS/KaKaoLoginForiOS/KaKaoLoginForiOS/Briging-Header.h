4c2d23c85178d672988d7bd54322cc60545fc60f
loveapplepi <loveapplepi@naver.com>
2019-06-12 09:36:04 +0900

    create kakao login for ios

---
+++ iOS/KaKaoLoginForiOS/KaKaoLoginForiOS/Briging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Briging-Header.h
+//  KaKaoLoginForiOS
+//
+//  Created by Pi on 11/06/2019.
+//  Copyright © 2019 applepi. All rights reserved.
+//
+
+#ifndef Briging_Header_h
+#define Briging_Header_h
+
+#import <KakaoOpenSDK/KakaoOpenSDK.h>
+
+
+#endif /* Briging_Header_h */
---
+++ iOS/KaKaoLoginForiOS/KaKaoLoginForiOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KaKaoLoginForiOS
+//
+//  Created by Pi on 11/06/2019.
+//  Copyright © 2019 applepi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
