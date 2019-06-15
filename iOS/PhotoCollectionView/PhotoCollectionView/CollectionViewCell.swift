b242ba4614cee02f0978820fcf590d10f0f22f82
loveapplepi <loveapplepi@naver.com>
2019-06-15 21:28:29 +0900

    add collectionview

---
+++ iOS/PhotoCollectionView/PhotoCollectionView/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  PhotoCollectionView
+//
+//  Created by Pi on 14/06/2019.
+//  Copyright © 2019 applepi. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
--- iOS/PhotoCollectionView/PhotoCollectionView/ViewController.swift
+++ iOS/PhotoCollectionView/PhotoCollectionView/ViewController.swift
@@ -10,11 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var collectionview: UICollectionView!
+    var images = [UIImage]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        collectionview.delegate = self
+        collectionview.dataSource = self
+        setImage()
     }
-
-
+    
+    private func setImage() {
+        for i in 1...7 {
+            images.append(UIImage(named: "\(i)")!)
+        }
+    
+    }
+    
+    
 }
 
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionview.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! CollectionViewCell
+        cell.imageView.image = images[indexPath.row]
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+
+        let collectionViewCellWithd = collectionView.frame.width / 3 - 1
+
+        return CGSize(width: collectionViewCellWithd, height: collectionViewCellWithd)
+
+    }
+    
+    
+    
+    
+    
+    
+}
