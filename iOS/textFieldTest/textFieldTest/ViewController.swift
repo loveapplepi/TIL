904f2c82b4fb58ea0ecf5bdabc58b450b202fd5a
loveapplepi <loveapplepi@naver.com>
2019-06-25 20:06:49 +0900

    update textFieldTest

--- iOS/textFieldTest/textFieldTest/ViewController.swift
+++ iOS/textFieldTest/textFieldTest/ViewController.swift
@@ -8,13 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        nameTextField.delegate = self
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
     }
+    
+    @objc func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        nameLabel.text = nameTextField.text
+        textField.resignFirstResponder()
+        
+        return true
+        
+    }
+    
+    @objc func keyboardWillShow(_ sender: Notification) {
+        
+        self.view.frame.origin.y = -150 // Move view 150 points upward
+        
+    }
+    
+    @objc func keyboardWillHide(_ sender: Notification) {
+        
+        self.view.frame.origin.y = 0 // Move view to original position
+        
+    }
+    
+    
+    
+    
 
-
+    @IBAction func modifyName(_ sender: Any) {
+        
+        
+    }
+    
 }
 
