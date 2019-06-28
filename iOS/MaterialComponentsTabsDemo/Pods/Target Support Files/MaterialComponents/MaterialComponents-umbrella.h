41259cb58a51adda680496238f6a8b0649cb198b
loveapplepi <loveapplepi@naver.com>
2019-06-28 14:56:39 +0900

    pod install

---
+++ iOS/MaterialComponentsTabsDemo/Pods/Target Support Files/MaterialComponents/MaterialComponents-umbrella.h
@@ -0,0 +1,20 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "MaterialPageControl.h"
+#import "MDCPageControl.h"
+#import "MaterialPalettes.h"
+#import "MDCPalettes.h"
+
+FOUNDATION_EXPORT double MaterialComponentsVersionNumber;
+FOUNDATION_EXPORT const unsigned char MaterialComponentsVersionString[];
+
