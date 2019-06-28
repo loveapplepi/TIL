c7e7c6e810a0fac2b4ac69a9378cc7fd1abad555
loveapplepi <loveapplepi@naver.com>
2019-06-28 09:51:03 +0900

    add page control

--- iOS/PageControlDemo/PageControlDemo/ViewController.swift
+++ iOS/PageControlDemo/PageControlDemo/ViewController.swift
@@ -7,14 +7,46 @@
 //
 
 import UIKit
+import MaterialComponents.MaterialPageControl
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIScrollViewDelegate {
 
+    let pageControl = MDCPageControl()
+    let scrollView = UIScrollView()
+    let pages = NSMutableArray()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        scrollView.delegate = self
+        view.addSubview(scrollView)
+        
+        pageControl.numberOfPages = 3
+        
+        let pageControlSize = pageControl.sizeThatFits(view.bounds.size)
+        pageControl.frame = CGRect(x: 0, y: view.bounds.height - pageControlSize.height, width: view.bounds.width, height: pageControlSize.height)
+        
+        pageControl.addTarget(self, action: #selector(didChangePage), for: .valueChanged)
+        pageControl.autoresizingMask = [.flexibleTopMargin, .flexibleWidth]
+        view.addSubview(pageControl)
     }
-
-
+    
+    @objc func didChangePage(sender: MDCPageControl){
+        var offset = scrollView.contentOffset
+        offset.x = CGFloat(sender.currentPage) * scrollView.bounds.size.width;
+        scrollView.setContentOffset(offset, animated: true)
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        pageControl.scrollViewDidScroll(scrollView)
+    }
+    
+    func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        pageControl.scrollViewDidEndDecelerating(scrollView)
+    }
+    
+    func scrollViewDidEndScrollingAnimation(_ scrollView: UIScrollView) {
+        pageControl.scrollViewDidEndScrollingAnimation(scrollView)
+    }
 }
 
