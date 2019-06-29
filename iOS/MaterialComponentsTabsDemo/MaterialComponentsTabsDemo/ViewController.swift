6faef93deac5f7393fbfabbb901d79483302f004
loveapplepi <loveapplepi@naver.com>
2019-06-29 19:27:50 +0900

    update tabBars

--- iOS/MaterialComponentsTabsDemo/MaterialComponentsTabsDemo/ViewController.swift
+++ iOS/MaterialComponentsTabsDemo/MaterialComponentsTabsDemo/ViewController.swift
@@ -7,14 +7,54 @@
 //
 
 import UIKit
+import MaterialComponents
 
 class ViewController: UIViewController {
+    
+    var tabBar: MDCTabBar!
+    
+//    func position(for bar: UIBarPositioning) -> UIBarPosition {
+//        return .topAttached
+//    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+//        tabBar.frame.origin = CGPoint.zero
+        
 
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        initTabBar()
     }
-
-
+    
+    func initTabBar() {
+        tabBar = MDCTabBar(frame: CGRect(origin: CGPoint(x: 0, y: 20), size: CGSize(width: view.bounds.width, height: view.bounds.height / 3)))
+        
+    
+        tabBar.items = [
+            UITabBarItem(title: "All\nhi", image: nil, tag: 0),
+            UITabBarItem(title: "My", image: nil, tag: 1),
+            UITabBarItem(title: "Archive", image: nil, tag: 2)
+        ]
+        
+        
+        
+        tabBar.tintColor = UIColor.black
+        
+        
+        
+        tabBar.selectedItemTintColor = UIColor.yellow
+        tabBar.unselectedItemTintColor = UIColor.black
+        tabBar.alignment = .justified
+        tabBar.itemAppearance = .titles
+        
+        tabBar.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+//        tabBar.sizeToFit()
+        tabBar.barTintColor = UIColor.red
+        view.addSubview(tabBar)
+    }
 }
-
