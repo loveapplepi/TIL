43cbc092694a4396449deaf5cedcd9440cc96960
loveapplepi <loveapplepi@naver.com>
2019-07-10 21:01:22 +0900

    registerForPushNotifications

--- iOS/RegisterPushNotificationDemo/RegisterPushNotificationDemo/AppDelegate.swift
+++ iOS/RegisterPushNotificationDemo/RegisterPushNotificationDemo/AppDelegate.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import UserNotifications
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +18,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        registerForPushNotifications()
+
         return true
     }
 
@@ -39,6 +43,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+    
+    func registerForPushNotifications() {
+        UNUserNotificationCenter.current() // 1
+            .requestAuthorization(options: [.alert, .sound, .badge]) { // 2
+                granted, error in
+                print("Permission granted: \(granted)") // 3
+        }
     }
 
 
