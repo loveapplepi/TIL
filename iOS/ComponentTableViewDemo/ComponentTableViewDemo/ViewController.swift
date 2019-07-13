6e66015ded1a290e8055bf79cc57414a633fd959
loveapplepi <loveapplepi@naver.com>
2019-07-13 22:45:20 +0900

    create component table view

---
+++ iOS/ComponentTableViewDemo/ComponentTableViewDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ComponentTableViewDemo
+//
+//  Created by Pi on 13/07/2019.
+//  Copyright © 2019 applepi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
