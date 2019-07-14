76e996a0516fa92e891948632dc3cd72be2574f0
loveapplepi <loveapplepi@naver.com>
2019-07-14 14:47:52 +0900

    update tableview

--- iOS/ComponentTableViewDemo/ComponentTableViewDemo/ViewController.swift
+++ iOS/ComponentTableViewDemo/ComponentTableViewDemo/ViewController.swift
@@ -17,12 +17,42 @@ class ViewController: UIViewController {
         scrollView.backgroundColor = .white
        return scrollView
     }()
+    
+    private let titleLabel = UILabel()
+    private let tableView = UITableView()
+    private var infoTitle = [String]()
+    
+    private func prpareProperties() {
+        
+    }
+    
+    private func configureProperties() {
+        view.insertSubview(scrollView, at: 0)
+        scrollView.snp.makeConstraints { make in
+            make.top.leading.trailing.bottom.equalToSuperview()
+        }
+        
+        scrollView.addSubview(titleLabel)
+        titleLabel.snp.makeConstraints { make in
+            make.leading.equalTo(self.view).offset(30)
+            make.top.equalTo(self.view).offset(145)
+        }
+    }
+    
+    private func setUI() {
+        prpareProperties()
+        configureProperties()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUI()
         // Do any additional setup after loading the view.
     }
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        scrollView.updateContentViewSize(height: 30)
+    }
 
 }
 
