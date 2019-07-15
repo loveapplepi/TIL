6b754bd5b3865dd78d4636e22f4ce27a2e71f37b
loveapplepi <loveapplepi@naver.com>
2019-07-15 18:50:34 +0900

    create text demo

---
+++ iOS/NSAttributeTextDemo/NSAttributeTextDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NSAttributeTextDemo
+//
+//  Created by Pi on 15/07/2019.
+//  Copyright © 2019 applepi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
