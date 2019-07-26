57ac4d2044ae8c997efb87cdcff0cc261d8f20a8
loveapplepi <loveapplepi@naver.com>
2019-07-26 17:59:47 +0900

    update demo

--- iOS/GustureTouchBeganDemo/GustureTouchBeganDemo/ViewController.swift
+++ iOS/GustureTouchBeganDemo/GustureTouchBeganDemo/ViewController.swift
@@ -9,12 +9,86 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var canvas: UIImageView!
+    
+    var brushColor = UIColor.black.cgColor
+    var brushWidth: CGFloat = 10.0
+    
+    var lastPoint = CGPoint.zero
+    var isDrawing = false
+    
+    var isPen = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    func drawLine(from: CGPoint, to: CGPoint) {
+        UIGraphicsBeginImageContext(canvas.frame.size)
+        
+        guard let context = UIGraphicsGetCurrentContext() else { return }
+        
+        canvas.image?.draw(in: canvas.bounds)
+        
+        if isPen {
+            context.setBlendMode(.normal)
+        } else {
+            context.setBlendMode(.clear)
+        }
+        
+        context.setLineCap(.round)
+        context.setLineWidth(brushWidth)
+        context.setStrokeColor(brushColor)
+        context.move(to: from)
+        context.addLine(to: to)
+        
+        context.strokePath()
+        
+        canvas.image = UIGraphicsGetImageFromCurrentImageContext()
+        
+        UIGraphicsEndImageContext()
+    }
+    
+    // MARK: - Touches
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        isDrawing = false
+        
+        if let touch = touches.first {
+            lastPoint = touch.location(in: canvas)
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        isDrawing = true
+        
+        if let touch = touches.first {
+            let currentPoint = touch.location(in: canvas)
+            
+            drawLine(from: lastPoint, to: currentPoint)
+            lastPoint = currentPoint
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if !isDrawing {
+            drawLine(from: lastPoint, to: lastPoint)
+        }
+        
+        isDrawing = false
+    }
+    
+    // MARK: - Actions
+    
+//    @IBAction func touchUpPencil(_ sender: Any) {
+//        isPen = true
+//    }
+//    
+//    @IBAction func touchUpEraser(_ sender: Any) {
+//        isPen = false
+//    }
+    
 }
 
